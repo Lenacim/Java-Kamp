186de7b7bfd7f48f77731459e53d33c67778c085
Aleyna Isnık <aleynaisnik@outlook.com>
2021-05-02 00:43:08 +0300

    interface

---
+++ interfaces/src/interfaces/CustomerManager.java
@@ -0,0 +1,33 @@
+package interfaces;
+
+public class CustomerManager {
+	
+	private Logger[] loggers;
+	
+	
+	
+	//loosly - tightly couped
+	
+	public CustomerManager(Logger[] loggers) {
+		this.loggers = loggers;
+	}
+	
+	public void add(Customer customer) {
+		System.out.println("Müþteri eklendi " + customer.getFirstName());
+		
+		Utils.runLoggers(loggers, customer.getFirstName());
+		
+	}
+	
+	
+	public void delete(Customer customer) {
+		System.out.println("Müþteri silindi" + customer.getFirstName());
+		
+		Utils.runLoggers(loggers, customer.getLastName());
+
+		
+	}
+	
+	
+
+}
